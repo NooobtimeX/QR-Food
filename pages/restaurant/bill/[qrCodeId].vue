e7ba051bff0299795d05b64acd5be73bf19b0f0b
NooobtimeX <nooobtimex@gmail.com>
2024-11-19 17:57:09 +0700

    lang

--- pages/restaurant/bill/[qrCodeId].vue
+++ pages/restaurant/bill/[qrCodeId].vue
@@ -132,10 +132,10 @@
               class="bg-green-500 px-4 py-2 text-white"
               @click="printPreview"
             >
-              Print
+              ปริ้น
             </button>
             <button class="bg-red-500 px-4 py-2" @click="closePrintPreview">
-              Cancel
+              ยกเลิก
             </button>
           </div>
         </div>
