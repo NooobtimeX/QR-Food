089ee846ff09d9f783dcc7f1e899a4a7588027be
NooobtimeX <nooobtimex@gmail.com>
2024-11-26 18:23:26 +0700

    logo

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,9 +1,12 @@
 <template>
   <nav class="bg-white p-6 shadow-xl">
     <div class="container mx-auto flex max-w-7xl items-center justify-between">
-      <h1 class="text-3xl font-bold text-black">
-        QR Food<span class="text-3xl text-orange-04">.</span>
-      </h1>
+      <div class="flex">
+        <img src="/logo/logo.png" alt="Logo" width="60px" />
+        <h1 class="m-auto text-3xl font-bold text-black">
+          QR Food<span class="text-3xl text-orange-04">.</span>
+        </h1>
+      </div>
       <button
         class="rounded-xl bg-red-500 p-2 text-white shadow-xl hover:bg-red-02"
         @click="signOut"
